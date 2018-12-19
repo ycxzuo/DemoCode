1cf09de3f8d1130212cb5ac88225b671d738fa29
zuoxin <343720929@qq.com>
2018-12-19 20:34:45 +0800

    delete JVM file

--- JVM/src/com/yczuoxin/jvm/demo/classloader/bean/SubClass.java
+++
@@ -1,7 +0,0 @@
-package com.yczuoxin.jvm.demo.classloader.bean;
-
-public class SubClass extends SuperClass {
-    static {
-        System.out.println("SubClass 初始化");
-    }
-}
--- JVM/src/com/yczuoxin/jvm/demo/classloader/bean/SuperClass.java
+++
@@ -1,8 +0,0 @@
-package com.yczuoxin.jvm.demo.classloader.bean;
-
-public class SuperClass {
-    static {
-        System.out.println("SuperClass 被初始化");
-    }
-    public static int value = 1;
-}
