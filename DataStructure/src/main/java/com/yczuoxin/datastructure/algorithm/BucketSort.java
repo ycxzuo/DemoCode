e834cc22740ee7d58b8a40492febe01dbafc2787
yczuoxin <343720929@qq.com>
2020-01-13 00:20:06 +0800

    桶排序

---
+++ DataStructure/src/main/java/com/yczuoxin/datastructure/algorithm/BucketSort.java
@@ -0,0 +1,47 @@
+package com.yczuoxin.datastructure.algorithm;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 桶排序
+ */
+public class BucketSort {
+
+    public static void main(String[] args) {
+        sort(Arrays.asList(5,2,4,6,8,1,7,3,9,2)).forEach(System.out::println);
+    }
+
+    private static List<Integer> sort(List<Integer> list) {
+        if (list.isEmpty()) {
+            return list;
+        }
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        int size = list.size();
+        List<Integer> result = new ArrayList<>(size);
+        // 找到最大值和最小值
+        for (int i = 0; i < size; i++) {
+            max = Math.max(max, list.get(i));
+            min = Math.min(min, list.get(i));
+        }
+        // 创建桶
+        int bucketNumber = (max - min) / size + 1;
+        List<List<Integer>> bucketList = new ArrayList<>(bucketNumber);
+        for (int i = 0; i < bucketNumber; i++) {
+            bucketList.add(new ArrayList<>());
+        }
+        // 放入桶
+        for (int i = 0; i < size; i++) {
+            bucketList.get((list.get(i) - min) / size).add(list.get(i));
+        }
+        // 存入结果集
+        for (int i = 0; i < bucketNumber; i++) {
+            Collections.sort(bucketList.get(i));
+            result.addAll(bucketList.get(i));
+        }
+        return result;
+    }
+}
