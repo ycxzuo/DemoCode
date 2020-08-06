a514663467592120c3bb0916ef8cb8a998ae8699
yczuoxin <343720929@qq.com>
2020-08-06 20:16:44 +0800

    提交demo代码

--- Concurrent/src/main/java/com/yczuoxin/concurrent/demo/basis/future/completable/MyCompletableFutureTest.java
+++ Concurrent/src/main/java/com/yczuoxin/concurrent/demo/basis/future/completable/MyCompletableFutureTest.java
@@ -6,52 +6,74 @@ public class MyCompletableFutureTest {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         //demo1();
         //demo2();
+        demo3();
+    }
+
+    /**
+     * 第二个方法可能是 main 线程执行或者 forkJoinPool 的线程执行
+     * 如果第一个任务线程执行完了，main 线程还没有走到第二个任务，则会由 main 线程执行
+     * 否则会有执行第一个任务的线程执行
+     *
+     * @throws InterruptedException
+     * @throws ExecutionException
+     */
+    private static void demo3() throws InterruptedException, ExecutionException {
         CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
-            System.out.println("执行第一个方法");
+            print("执行第一个方法: " + Thread.currentThread().getName());
+            //int i = 1/0;
             sleep(1);
             return "task1";
         });
-        future.whenComplete((f, e) -> {
+        CompletableFuture<String> future1 = future.whenComplete((f, e) -> {
             sleep(1);
-            System.out.println("第一个方法的返回值: " + f);
-            System.out.println(e);
+            print("执行第二个方法: " + Thread.currentThread().getName());
+            print("第一个方法的返回值: " + f);
+            print(e.getMessage());
         });
+        print("main");
+        print(future1.get());
     }
 
+    /**
+     * 默认使用的线程是 forkJoinPool 获取的，并且是守护线程
+     *
+     * @throws InterruptedException
+     * @throws ExecutionException
+     */
     private static void demo2() throws InterruptedException, ExecutionException {
         ExecutorService executorService = Executors.newCachedThreadPool();
         executorService.submit(() -> {
-            System.out.println("executor service 是否是守护线程 :" + Thread.currentThread().isDaemon());
+            print("executor service 是否是守护线程 :" + Thread.currentThread().isDaemon());
             return null;
         });
         CompletableFuture<String> future= CompletableFuture.supplyAsync(() -> {
-            System.out.println("this is lambda supplyAsync");
-            System.out.println("supplyAsync lambda 是否是守护线程 :" + Thread.currentThread().isDaemon());
-            System.out.println("supplyAsync lambda Thread Name :" + Thread.currentThread().getName() + ", " + Thread.currentThread().getThreadGroup().getName());
+            print("this is lambda supplyAsync");
+            print("supplyAsync lambda 是否是守护线程 :" + Thread.currentThread().isDaemon());
+            print("supplyAsync lambda Thread Name :" + Thread.currentThread().getName() + ", " + Thread.currentThread().getThreadGroup().getName());
             sleep(2);
-            System.out.println("this lambda is executed by forkJoinPool");
+            print("this lambda is executed by forkJoinPool");
             return "result1";
         });
         CompletableFuture<String> future2 = CompletableFuture.supplyAsync(() -> {
-            System.out.println("this is executor service supplyAsync");
-            System.out.println("supplyAsync executor service 是否是守护线程 :" + Thread.currentThread().isDaemon());
-            System.out.println("supplyAsync executor service Thread Name :" + Thread.currentThread().getName() + ", " + Thread.currentThread().getThreadGroup().getName());
+            print("this is executor service supplyAsync");
+            print("supplyAsync executor service 是否是守护线程 :" + Thread.currentThread().isDaemon());
+            print("supplyAsync executor service Thread Name :" + Thread.currentThread().getName() + ", " + Thread.currentThread().getThreadGroup().getName());
             return "result2";
         }, executorService);
         CompletableFuture<Void> future3 = CompletableFuture.allOf(future, future2);
         future3.get();
-        //System.out.println(future2.get());
-        //System.out.println(future.get());
+        //print(future2.get());
+        //print(future.get());
         executorService.shutdown();
     }
 
     private static void demo1() throws InterruptedException, ExecutionException {
         CompletableFuture<String> future = new CompletableFuture<>();
         new Thread(() -> {
-            System.out.println(Thread.currentThread().getName() + ": CompletableFuture 可以监控这个任务的执行");
+            print(Thread.currentThread().getName() + ": CompletableFuture 可以监控这个任务的执行");
             future.complete("任务返回结果");
         }).start();
-        System.out.println(future.get());
+        print(future.get());
     }
 
 
@@ -61,5 +83,9 @@ public class MyCompletableFutureTest {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void print(String message) {
+        System.out.println(message);
     }
 }
