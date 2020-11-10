c0f1df16dafc0a4df8b31c26168347bc0ad40cff
yczuoxin <343720929@qq.com>
2020-11-10 23:38:47 +0800

    lettuce 代码提交,但是没有设置序列化方式，导致前面会带一部分乱码

---
+++ redis-demo/lettuce-demo/src/main/java/com/yczuoxin/lettucedemo/controller/LettuceController.java
@@ -0,0 +1,26 @@
+package com.yczuoxin.lettucedemo.controller;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LettuceController {
+
+    private RedisTemplate<Object, Object> redisTemplate;
+
+    public LettuceController(RedisTemplate<Object, Object> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    @GetMapping("/redis/{key}")
+    public String getFromLettuce(@PathVariable("key") String key) {
+        if (redisTemplate.hasKey(key)) {
+            return redisTemplate.opsForValue().get(key).toString();
+        }
+        redisTemplate.opsForValue().set(key, "123");
+        return "Redis 没有存储这个 key";
+    }
+
+}
---
+++ redis-demo/lettuce-demo/src/test/java/com/yczuoxin/lettucedemo/LettuceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yczuoxin.lettucedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LettuceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
