16f8fa0ec845415eec98953b8b8cc029d274f314
yczuoxin <343720929@qq.com>
2020-11-13 21:43:58 +0800

    lettuce 代码提交，修改序列化方式

--- redis-demo/lettuce-demo/src/main/java/com/yczuoxin/lettucedemo/controller/LettuceController.java
+++ redis-demo/lettuce-demo/src/main/java/com/yczuoxin/lettucedemo/controller/LettuceController.java
@@ -1,14 +1,20 @@
 package com.yczuoxin.lettucedemo.controller;
 
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 @RestController
 public class LettuceController {
 
     private RedisTemplate<String, Object> redisTemplate;
+
+    @Resource(name = "redisTemplate")
+    private ValueOperations<String, String> value;
 
     public LettuceController(RedisTemplate<String, Object> redisTemplate) {
         this.redisTemplate = redisTemplate;
@@ -17,7 +23,7 @@ public class LettuceController {
     @GetMapping("/redis/{key}")
     public String getFromLettuce(@PathVariable("key") String key) {
         if (redisTemplate.hasKey(key)) {
-            return redisTemplate.opsForValue().get(key).toString();
+            return value.get(key);
         }
         redisTemplate.opsForValue().set(key, "123");
         return "Redis 没有存储这个 key";
